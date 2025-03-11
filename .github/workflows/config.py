0ccab653f5c7f2db1ae2dc3794bb048f049d88ef
Rockybhai21 <rockybhai2109rs@gmail.com>
2025-03-11 19:45:05 +0530

    Update config.py

--- .github/workflows/config.py
+++ .github/workflows/config.py
@@ -7,9 +7,9 @@ from os import environ
 class Config:
     API_ID = int(environ.get("API_ID", "27317669"))
     API_HASH = environ.get("API_HASH", "11b88c331c5d44fde57cf91de1a2156b")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "7195946309:AAFb35tm6ZRxf-OajZuDfFqw4iFtqIs3ClQ") 
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7890986836:AAG5sd3BW8MpP-_X1q7QGNtAbXJdvN_J-HA") 
     BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
-    DATABASE_URI = environ.get("DATABASE_URI", "")
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://beanonymoushacker09:q9gxQwIZDCmVOycI@cluster0.ej0gk.mongodb.net/?retryWrites=true&w=majority&appName =Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "vj-forward-bot")
     BOT_OWNER = int(environ.get("BOT_OWNER", "6947378236"))
 
